e34aceb5048dec7dc6a0cd0be06e10e624e5a257
zohary89 <71074080+zohary89@users.noreply.github.com>
2021-01-05 00:36:27 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
-from albumsearcher.app import app
+import os
+
+from albumsearcher.app import create_app
 from albumsearcher.db import db
 
 
 if __name__ == '__main__':
+    configs = {
+        'SQLALCHEMY_DATABASE_URI': os.environ.get('DATABASE_URL') or 'sqlite://',
+        'SECRET_KEY': os.environ.get('SECRET_KEY') or "SECRET"
+    }
+    app = create_app(**configs)
     with app.app_context():
         db.create_all()
 
